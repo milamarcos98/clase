49c9ac1a02af43cf00997b807e82635015a251c9
mila <milamarcos98@gmail.com>
2024-08-17 21:07:55 -0300

    name fix

---
+++ index.js
@@ -0,0 +1,37 @@
+// // CommonJS
+// function suma(a,b){
+//     return a + b;
+// }
+
+// function resta(a,b){
+//     return a - b;
+// }
+
+// // module.exports 
+
+// // module.exports = suma;
+
+
+// // exports
+
+// exports.suma = suma;
+// exports.resta = resta;
+
+
+
+
+// ES MODULES
+
+export function suma(a,b){
+    return a + b;
+}
+
+export function resta(a,b){
+    return a - b;
+}
+
+// export {suma, resta};
+
+// export default function suma(a,b){
+//     return a + b;
+// }
